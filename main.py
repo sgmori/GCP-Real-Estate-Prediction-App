796cbd50fb8e039ffc785365f1d32303dff907b9
Samuel Mori <49626895+sgmori@users.noreply.github.com>
2019-06-14 13:05:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-# [START gae_python37_render_template]
-import datetime
+from Flask import Flask
+from google.cloud import bigquery
 
-from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def predict_model():
+def hello():
     client = bigquery.Client()
     sql = """
     #standardSQL
@@ -58,12 +57,5 @@ def predict_model():
 
 
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
+
     app.run(host='127.0.0.1', port=8080, debug=True)
-# [START gae_python37_render_template]
