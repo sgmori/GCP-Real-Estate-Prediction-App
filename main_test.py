703d0d02bf6984371368ccd2c624f132a13ac7f0
Samuel Mori <49626895+sgmori@users.noreply.github.com>
2019-06-14 20:02:24 -0700

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -13,7 +13,7 @@ def flaskclient():
     return main.app.test_client()
 
 
-def test_main(flaskclient):
-    r = flaskclient.get("/")
+def test_main(flask_client):
+    r = flask_client.get("/")
     assert r.status_code == 302
     assert "/results" in r.headers.get("location", "")
