d60990ee79d8ea9cb84bad7087b515dd8b3dfdbc
Samuel Mori <49626895+sgmori@users.noreply.github.com>
2019-06-14 19:34:24 -0700

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -1,17 +1,3 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import concurrent.futures
 from unittest import mock
 
@@ -54,20 +40,3 @@ def test_results(flask_client, monkeypatch):
     assert "Query Result" in response_body  # verifies header
     assert "example2.com" in response_body
     assert "42" in response_body
-
-
-def test_results_timeout(flask_client, monkeypatch):
-    import main
-
-    fake_job = mock.create_autospec(bigquery.QueryJob)
-    fake_job.result.side_effect = concurrent.futures.TimeoutError()
-
-    def fake_get_job(self, job_id, **kwargs):
-        return fake_job
-
-    monkeypatch.setattr(main.bigquery.Client, "get_job", fake_get_job)
-
-    r = flask_client.get("/results", follow_redirects=True)
-
-    assert r.status_code == 200
-    assert "Query Timeout" in r.data.decode("utf-8")
