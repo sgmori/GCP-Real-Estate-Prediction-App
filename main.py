23655664f98d93a23fb4f7106617969540366339
Samuel Mori <49626895+sgmori@users.noreply.github.com>
2019-06-14 19:33:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START gae_python37_bigquery]
-import concurrent.futures
-
 import flask
 from google.cloud import bigquery
 
@@ -27,16 +10,45 @@ bigquery_client = bigquery.Client()
 def main():
     query_job = bigquery_client.query(
         """
+        #standardSQL
+        SELECT *
+        FROM ml.PREDICT(
+            MODEL `msds-434-analytics-app.house_analytics`.house_price_model2,
+        (
+
+        #standardSQL
+        WITH params AS (
+            SELECT
+            1 AS TRAIN,
+            2 AS EVAL
+            ),
+
+        house_sales AS (
         SELECT
-        CONCAT(
-            'https://stackoverflow.com/questions/',
-            CAST(id as STRING)) as url,
-        view_count
-        FROM `bigquery-public-data.stackoverflow.posts_questions`
-        WHERE tags like '%google-bigquery%'
-        ORDER BY view_count DESC
-        LIMIT 10
-    """
+            bedrooms,
+            bathrooms,
+            sqft_living,
+            sqft_lot,
+            waterfront,
+            condition,
+            grade,
+            yr_built,
+            zipcode,
+            price
+        FROM
+            `msds-434-analytics-app.house_analytics.kc_house_data_clean`,
+            params
+        WHERE
+            price > 0 AND bedrooms < 6
+            AND MOD(ABS(FARM_FINGERPRINT(
+                CAST(id AS STRING))),1000) = params.EVAL
+        )
+
+
+        SELECT *
+        FROM house_sales
+        ))
+        """
     )
 
     return flask.redirect(
@@ -61,18 +73,11 @@ def results():
         location=location,
     )
 
-    try:
-        # Set a timeout because queries could take longer than one minute.
-        results = query_job.result(timeout=30)
-    except concurrent.futures.TimeoutError:
-        return flask.render_template("timeout.html", job_id=query_job.job_id)
+    results = query_job.result(timeout=30)
 
     return flask.render_template("query_result.html", results=results)
 
 
 if __name__ == "__main__":
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
+
     app.run(host="127.0.0.1", port=8080, debug=True)
-# [END gae_python37_bigquery]
